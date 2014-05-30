2cb223caa9b42bd0042994077fd9ee0cf72d110f
lihuanglx <lihuanglx@gmail.com>
2014-05-30 21:47:32 +0800

    增加管理员函数

--- application/models/Funds_Account_Admin.php
+++ application/models/Funds_Account_Admin.php
@@ -32,9 +32,28 @@
 			$this->db->query($sql);	
 		}
 
-		// 列出等待确认的开户信息
+		// 列出等待审核的开户信息
+		// 返回数组，每行一条
 		public function get_register_list() {
+			$sql = "SELECT * FROM funds_account WHERE create_state=0";
+			$query = $this->db->query($sql);
+			return $query->result_array();
+		}
 
+		// 列出等待审核的挂失信息
+		// 返回数组，每行一条
+		public function get_lost_list() {
+			$sql = "SELECT * FROM lost_application WHERE state=0"
+			$query = $this->db->query($sql);
+			return $query->result_array();
+		}
+
+		// 列出等待审核的销户信息
+		// 返回数组，每行一条
+		public function get_cancel_list() {
+			$sql = "SELECT * FROM cancel_application WHERE state=0"
+			$query = $this->db->query($sql);
+			return $query->result_array();
 		}
 	}
 ?>
